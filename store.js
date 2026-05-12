a4fc954155e7a6317b46a82d1d3d5bb833e95091
Wai-Min-Hein-CBS <wai06276@gmail.com>
2026-05-12 17:53:20 +0630

    [feat]: Protect passwords in localStorage using salted hash

--- store.js
+++ store.js
@@ -12,12 +12,35 @@ function findUser(username) {
   }) || null;
 }
 
-function addUser(displayName, username, password) {
+function bytesToHex(bytes) {
+  return Array.from(bytes).map(function(byte) {
+    return byte.toString(16).padStart(2, '0');
+  }).join('');
+}
+
+function createSalt() {
+  var bytes = new Uint8Array(16);
+  crypto.getRandomValues(bytes);
+  return bytesToHex(bytes);
+}
+
+async function hashPassword(password, salt) {
+  var encoder = new TextEncoder();
+  var data = encoder.encode(salt + password);
+  var hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  return bytesToHex(new Uint8Array(hashBuffer));
+}
+
+async function addUser(displayName, username, password) {
   var users = getUsers();
+  var salt = createSalt();
+  var hash = await hashPassword(password, salt);
+
   users.push({
     displayName: displayName,
     username: username,
-    password: password
+    salt: salt,
+    hash: hash
   });
   saveUsers(users);
 }
